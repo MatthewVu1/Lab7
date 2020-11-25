dad22bf074e2c18f73803085ab7d028eadf0433d
Matthew <mvu19@my.bcit.ca>
2020-11-24 18:49:58 -0800

    added javascript code to add the inputted number to the html

--- js/script.js
+++ js/script.js
@@ -1,44 +1,81 @@
-class1 = {
-    code: "ACIT-1620",
-    number: "1620",
-    name: "Web Fundamental Technologies"
-};
-class2 = {
-    code: "ACIT-1515",
-    number: "1515",
-    name: "Scripting for IT"
-};
-class3 = {
-    code: "COMM-1116",
-    number: "1116",
-    name: "Business Communications 1"
-};
-const courseList = [class1, class2, class3];
+function createCourseArray(){
+    // This function doesn't actually read the document, so it needs to be fixed, as I haven't figured it out.
+    // So it is just hardcoded in
+    class1 = {
+        code: "ACIT-1620",
+        name: "Web Fundamental Technologies"
+    };
+    class2 = {
+        code: "ACIT-1515",
+        name: "Scripting for IT"
+    };
+    class3 = {
+        code: "COMM-1116",
+        name: "Business Communications 1"
+    };
+    const courseList = [class1, class2, class3];
+    return courseList
+    }
 
-do {
-    input = prompt("Enter a 4 digit code:")
-}
-while (isNaN(input) || input.length != 4)
+function findCourse(courseList){
+    for (let course of courseList) {
+        if (course.code.includes(input)) {
+            console.log(`Yes I am taking the course: ${course.code} - ${course.name}`);
+            if (course === class1)
+                document.getElementById("class1").style.backgroundColor = "lightgreen";
+            if (course === class2)
+                document.getElementById("class2").style.backgroundColor = "lightgreen";
+            if (course === class3)
+                document.getElementById("class3").style.backgroundColor = "lightgreen";
+            valid = true;
+            break;
+        } else {
+            valid = false
+        }
+    }
+    if (valid === false) {
+        class4 = {
+            code: `${input}`,
+            name: "Null"
+        }
+        courseList.push(class4);
+        console.log("New object sucessfully added")
+        var element = document.getElementById("course_list");
+
+        var line = document.createElement("hr");
+        element.appendChild(line)
+
+        var new_course = document.createElement("div")
+        new_course.className = "course";
+        element.appendChild(new_course)
 
 
+        var code = document.createElement("div");
+        var node = document.createTextNode(`${input}`);
+        code.appendChild(node);
+        new_course.appendChild(code);
 
-for (let course of courseList) {
-    if (input === course.number) {
-        console.log(`Yes I am taking the course: ${course.code} - ${course.name}`)
-        valid = true
-        break;
-    } else {
-        valid = false
+        var desc = document.createElement("p");
+        var node_desc = document.createTextNode("N/A");
+        desc.appendChild(node_desc);
+        new_course.appendChild(desc)
+
+        var date = document.createElement("div");
+        var node_date = document.createTextNode("Fall 2020");
+        date.className = "course_desc";
+        date.appendChild(node_date);
+        element.appendChild(date)
     }
 }
-if (valid === false) {
-    class4 = {
-        code: `${input}`,
-        number: `${input}`,
-        name: "Null"
+
+function main(){
+    do {
+        input = prompt("Enter a 4 digit code:")
     }
-    courseList.push(class4);
-    console.log("New object sucessfully added")
+    while (isNaN(input) || input.length != 4)
+
+    course = createCourseArray()
+    findCourse(course)
 }
 
-
+main()
